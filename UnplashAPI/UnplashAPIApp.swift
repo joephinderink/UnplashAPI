436bd536b3d58ad5e8905fb699b4b0e22d71ef2d
Joep Hinderink <hello@joephinderink.nl>
2021-01-24 00:31:02 +0100

    Initial Commit

---
+++ UnplashAPI/UnplashAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnplashAPIApp.swift
+//  UnplashAPI
+//
+//  Created by Joep Hinderink on 24/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct UnplashAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
